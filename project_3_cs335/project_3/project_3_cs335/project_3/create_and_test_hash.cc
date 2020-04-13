5688731493d9734e5f6e7346f942baa284157a10
JonaChen98 <48601138+JonaChen98@users.noreply.github.com>
2020-04-13 18:31:15 -0400

    Add files via upload

---
+++ project_3_cs335/project_3/project_3_cs335/project_3/create_and_test_hash.cc
@@ -0,0 +1,99 @@
+// YOUR NAME.
+
+
+#include "quadratic_probing.h"
+#include "linear_probing.h"
+#include "double_hashing.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <fstream>
+
+
+using namespace std;
+
+
+template <typename HashTableType>
+void TestFunctionForHashTable(HashTableType &hash_table, const string &words_filename, const string &query_filename) {
+  cout << "TestFunctionForHashTables..." << endl;
+  cout << "Words filename: " << words_filename << endl;
+  cout << "Query filename: " << query_filename << endl;
+  hash_table.MakeEmpty();  
+  // insert own code... so you want to insert from words.txt and find from querywords.txt
+  ifstream words_file, query;
+  string line;
+  int word_count =0;
+  words_file.open(words_filename);
+  if(!words_file) {
+    cout << "err opening file" << endl;
+    exit(1);
+  }
+
+  while(getline(words_file, line)) {
+    hash_table.Insert(line);
+    word_count++;
+  }
+
+  if(hash_table.r_value == 73){
+    cout << "r_value: " << hash_table.r_value << endl;
+  }
+  cout << "Number of items: " << hash_table.Num_elements() << endl;
+  cout << "Size of hash table: " << hash_table.table_size()<< endl;
+  float load_factor = ((float)hash_table.Num_elements() / hash_table.table_size());
+  cout << "Load factor: "<< load_factor << endl; // number of elements/tablesize
+  cout << "Collisions: " << hash_table.Public_Count_Collisions() << endl;
+  float avg_collision =( hash_table.Public_Count_Collisions()/(float)hash_table.Num_elements());
+  cout << "Avg. number of collisions: " << avg_collision << endl; //number of collisions/ number of elements in table (current size)
+
+  words_file.close();
+  query.open(query_filename);
+  if(!query){
+    cout<<"err opening file" << endl;
+    exit(1);
+  }
+
+  while(getline(query,line)){
+    if(hash_table.Contains(line) == true){
+      cout << line << " Found " <<hash_table.public_get_probes(line) << endl;
+    }
+    else{
+      cout << line << " Not Found " << hash_table.public_get_probes(line) <<endl;
+    }
+  }
+}
+
+int testFunctionWrapper(int argument_count, char **argument_list) {
+  const string words_filename(argument_list[1]);
+  const string query_filename(argument_list[2]);
+  const string param_flag(argument_list[3]);
+
+  if (param_flag == "linear") {
+    HashTableLinear<string> linear_probing_table;
+    TestFunctionForHashTable(linear_probing_table, words_filename, query_filename);    
+  } else if (param_flag == "quadratic") {
+    HashTable<string> quadratic_probing_table;
+    TestFunctionForHashTable(quadratic_probing_table, words_filename, query_filename);    
+  } else if (param_flag == "double") {
+    HashTableDouble<string> double_probing_table;
+    TestFunctionForHashTable(double_probing_table, words_filename, query_filename);    
+  } else {
+    cout << "Unknown tree type " << param_flag << " (User should provide linear, quadratic, or double)" << endl;
+  }
+  return 0;
+}
+
+// Sample main for program create_and_test_hash.
+// WE WILL NOT USE YOUR MAIN IN TESTING. DO NOT CODE FUNCTIONALITY INTO THE MAIN.
+// WE WILL DIRECTLY CALL testFunctionWrapper. ALL FUNCTIONALITY SHOULD BE THERE.
+// This main is only here for your own testing purposes.
+int main(int argc, char **argv) {
+
+  if (argc != 4) {
+    cout << "Usage: " << argv[0] << " <wordsfilename> <queryfilename> <flag>" << endl;
+    return 0;
+  }
+  
+  testFunctionWrapper(argc, argv);
+  return 0;
+}
---
+++ project_3_cs335/project_3/project_3_cs335/project_3/spell_check.cc
@@ -0,0 +1,74 @@
+// YOUR NAME.
+
+#include "quadratic_probing.h"
+#include "linear_probing.h"
+#include "double_hashing.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int testSpellingWrapper(int argument_count, char** argument_list) {
+
+  const string document_filename(argument_list[1]);
+  const string dictionary_filename(argument_list[2]);
+  
+  cout << "Input document filename is " << document_filename << endl;
+  cout << "Input dictionary filename is " << dictionary_filename << endl;
+
+  // Call functions implementing the assignment requirements.
+  HashTable<string> Dictionary_table;
+  Dictionary_table.MakeEmpty();
+
+  string diction_word;
+  string document_word;
+  ifstream document_stream;
+  ifstream dictionary_stream;
+
+  document_stream.open(document_filename);
+    if(document_stream.fail()){
+      cerr << "error on opening";
+  }
+
+  dictionary_stream.open(dictionary_filename);
+    if(dictionary_stream.fail()){
+      cerr << "error on opening";
+  }
+  // insert all dictionary words into quadratic hashtable
+  while(getline(dictionary_stream,diction_word)){
+    Dictionary_table.Insert(diction_word);
+  }
+
+  //implementation for reading each line in document file and then checking
+  //if containts in the hashtable
+  while(getline(document_stream,document_word)){
+     //if contains then just print <word1> is CORRECT
+    for(int i = 0; i < document_word.length(); i++){
+      document_word[i] = tolower(document_word[i]);
+    }
+    
+    if(Dictionary_table.Contains(document_word)){
+      cout << document_word << " is CORRECT" << endl;
+    }
+  }
+  //if not do error checkings
+  //a) Adding one character in any possible position
+  //b) Removing one character from the word
+  //c) Swapping adjacent characters in the word
+} 
+
+
+// Sample main for program spell_check.
+// WE WILL NOT USE YOUR MAIN IN TESTING. DO NOT CODE FUNCTIONALITY INTO THE MAIN.
+// WE WILL DIRECTLY CALL testSpellingWrapper. ALL FUNCTIONALITY SHOULD BE THERE.
+// This main is only here for your own testing purposes.
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    cout << "Usage: " << argv[0] << " <document-file> <dictionary-file>" << endl;
+    return 0;
+  }
+  
+  testSpellingWrapper(argc, argv);
+
+  return 0;
+}
